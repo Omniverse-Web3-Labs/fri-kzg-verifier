e00ca278375e84c35325ca58f1be05b1054e24df
xiyu1984 <shanellyy@126.com>
2024-03-17 21:18:43 +0800

    zk 6358 circuit verified

--- READEME.md
+++ READEME.md
@@ -5,6 +5,8 @@
 ```sh
 
 # mock_verifier
-cargo test -r --package fri-kzg-verifier --lib -- exec::mock_verifier::tests::test_sc_fir_kzg_verifier_evm --exact --nocapture
+cargo test -r --package fri-kzg-verifier --lib -- exec::mock_verifier::tests::test_sc_fir_kzg_verifier_evm --exact --nocapture --ignored
+
+cargo test -r --package fri-kzg-verifier --lib -- exec::mock_verifier::tests::test_evm_verifier_from_local --exact --nocapture --ignored
 
 ```
--- src/exec/mock_verifier.rs
+++ src/exec/mock_verifier.rs
@@ -2,11 +2,12 @@
 #[cfg(test)]
 pub mod tests {
     use anyhow::{Ok, Result};
-    use circuit_local_storage::circuit::{p_v_io::proof_tuple_to_local, vd_local::write_circuit_p_v_data_to_local};
+    use circuit_local_storage::circuit::{p_v_io::{proof_tuple_to_local, read_compressed_ppis_from_local, read_ppis_from_local, PVDataPath}, vd_local::write_circuit_p_v_data_to_local};
+    use client_verifier::circuit::verify_from_file::PureVerifier;
     use itertools::Itertools;
 
     use log::{info, LevelFilter};
-    use plonky2::{field::extension::Extendable, fri::FriConfig, hash::{hash_types::{HashOut, RichField}, merkle_tree::MerkleTree, poseidon::PoseidonHash}, iop::witness::{PartialWitness, WitnessWrite}, plonk::{circuit_builder::CircuitBuilder, circuit_data::CircuitConfig, config::{GenericConfig, GenericHashOut, PoseidonGoldilocksConfig}}};
+    use plonky2::{field::extension::Extendable, fri::FriConfig, hash::{hash_types::{HashOut, RichField}, merkle_tree::MerkleTree, poseidon::PoseidonHash}, iop::witness::{PartialWitness, WitnessWrite}, plonk::{circuit_builder::CircuitBuilder, circuit_data::{CircuitConfig, VerifierCircuitData}, config::{GenericConfig, GenericHashOut, PoseidonGoldilocksConfig}}};
     use plonky2_ecdsa::gadgets::recursive_proof::{self, recursive_proof_2};
     use semaphore_aggregation::plonky2_verifier::{bn245_poseidon::plonky2_config::{standard_inner_stark_verifier_config, standard_stark_verifier_config, Bn254PoseidonGoldilocksConfig}, verifier_api::verify_inside_snark};
     use zk_6358_prover::circuit::signature_prover::build_merkle_root_chip;
@@ -53,6 +54,7 @@ pub mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_sc_fir_kzg_verifier_evm() {
         let mut log_builder = env_logger::Builder::from_default_env();
         log_builder.format_timestamp(None);
@@ -90,6 +92,39 @@ pub mod tests {
         proof_tuple_to_local("s_smt_sv", &final_proof, true).unwrap();
 
         write_circuit_p_v_data_to_local("s_smt_sv", &final_proof).unwrap();
+
+        info!("start verify in snark");
+        verify_inside_snark(21, final_proof);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_evm_verifier_from_local() {
+        let mut log_builder = env_logger::Builder::from_default_env();
+        log_builder.format_timestamp(None);
+        log_builder.filter_level(LevelFilter::Info);
+        log_builder.try_init().unwrap();
+
+        const D: usize = 2;
+        type INNERC = PoseidonGoldilocksConfig;
+        type STRKC = Bn254PoseidonGoldilocksConfig;
+        type F = <INNERC as GenericConfig<D>>::F;
+
+        let pv_path = PVDataPath::new("st_parallel_utxo_high_rate");
+
+        let vd = VerifierCircuitData::<F, INNERC, D>::load_from_file(&pv_path.verifier_only_path, &pv_path.common_path);
+
+        let ppis = read_ppis_from_local::<F, INNERC, D>(&pv_path.ppis_path);
+        // let c_ppis = read_compressed_ppis_from_local::<F, INNERC, D>(&pv_path.compressed_ppis_path);
+
+        // vd.verify(ppis).unwrap();
+        // vd.verify_compressed(c_ppis).unwrap();
+
+        let high_rate_proof = (ppis, vd.verifier_only, vd.common);
+
+        let starky_config = standard_stark_verifier_config();
+
+        let final_proof = recursive_proof_2::<F, STRKC, INNERC, D>(&vec![high_rate_proof], &starky_config, None).unwrap();
 
         info!("start verify in snark");
         verify_inside_snark(21, final_proof);
